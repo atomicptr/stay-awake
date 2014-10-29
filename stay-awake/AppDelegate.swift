d3c358c3836778733159c0835a3d2fb883246795
Christopher Kaster <ikasoki@gmail.com>
2014-10-29 20:37:14 +0100

    Initial Commit

---
+++ stay-awake/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  stay-awake
+//
+//  Created by Christopher Kaster on 29/10/14.
+//  Copyright (c) 2014 Christopher Kaster. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ stay-awakeTests/stay_awakeTests.swift
@@ -0,0 +1,36 @@
+//
+//  stay_awakeTests.swift
+//  stay-awakeTests
+//
+//  Created by Christopher Kaster on 29/10/14.
+//  Copyright (c) 2014 Christopher Kaster. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class stay_awakeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
