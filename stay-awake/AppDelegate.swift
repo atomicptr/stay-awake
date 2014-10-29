c68536d5aa1774161321414e9bbecf787aaae577
Christopher Kaster <ikasoki@gmail.com>
2014-10-29 21:08:45 +0100

    call method when left click, show menu otherwise

--- stay-awake/AppDelegate.swift
+++ stay-awake/AppDelegate.swift
@@ -1,24 +1,36 @@
-//
-//  AppDelegate.swift
-//  stay-awake
-//
-//  Created by Christopher Kaster on 29/10/14.
-//  Copyright (c) 2014 Christopher Kaster. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
 
     @IBOutlet weak var statusMenu:NSMenu!
     
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
+        statusItem.title = "Stay Awake"
+        
+        statusMenu.delegate = self
+        
+        statusItem.menu = statusMenu
+    }
+    
+    func applicationWillTerminate(aNotification: NSNotification) {
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
+    func menuWillOpen(menu:NSMenu) {
+        let mouseButton = NSEvent.pressedMouseButtons()
+        
+        if mouseButton == 1 {
+            // don't open menu
+            menu.cancelTracking()
+            
+            menuItemClicked()
+        }
+    }
+    
+    func menuItemClicked() {
+        println("Test")
     }
 
     @IBAction func aboutMenuItemClicked(sender: AnyObject) {
