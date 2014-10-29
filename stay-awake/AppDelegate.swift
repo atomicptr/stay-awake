f0892da74a09229e1201a842777173cd1c5e7ee5
Christopher Kaster <ikasoki@gmail.com>
2014-10-29 21:31:30 +0100

    add enableInsomnia and disableInsomnia functions

--- stay-awake/AppDelegate.swift
+++ stay-awake/AppDelegate.swift
@@ -5,7 +5,12 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
 
     @IBOutlet weak var statusMenu:NSMenu!
     
+    let LEFT_MOUSE_BUTTON = 1
+    
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    
+    var task = NSTask()
+    var stayAwake = false
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         statusItem.title = "Stay Awake"
@@ -16,12 +21,13 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     }
     
     func applicationWillTerminate(aNotification: NSNotification) {
+        disableInsomnia()
     }
 
     func menuWillOpen(menu:NSMenu) {
         let mouseButton = NSEvent.pressedMouseButtons()
         
-        if mouseButton == 1 {
+        if mouseButton == LEFT_MOUSE_BUTTON {
             // don't open menu
             menu.cancelTracking()
             
@@ -30,13 +36,41 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     }
     
     func menuItemClicked() {
-        println("Test")
+        if(!stayAwake) {
+            enableInsomnia()
+            statusItem.title = "Awake"
+        } else {
+            disableInsomnia()
+            statusItem.title = "Sleepy"
+        }
+        
     }
 
-    @IBAction func aboutMenuItemClicked(sender: AnyObject) {
-    }
+    @IBAction func aboutMenuItemClicked(sender: AnyObject) {}
 
     @IBAction func quitMenuItemClicked(sender: AnyObject) {
+        disableInsomnia()
+        exit(0)
+    }
+    
+    func enableInsomnia() {
+        stayAwake = true
+        
+        task = NSTask()
+        
+        task.launchPath = "/usr/bin/caffeinate"
+        // -d prevent display sleep
+        // -i prevent system idle sleep
+        // -m prevent disk idle sleep
+        task.arguments = ["-d", "-i", "-m"]
+        
+        task.launch()
+    }
+    
+    func disableInsomnia() {
+        stayAwake = false
+        
+        task.interrupt()
     }
 
 }
